604a7dedd4ceb4acc18820e6ed842bccf8f0286a
Masoud Mahmoodi <mrm7g8@gmail.com>
2026-03-25 15:15:46 -0700

    feat(auth): add protected frontend routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,49 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ApplicationsPage from "./pages/ApplicationsPage.jsx";
 import NewApplicationPage from "./pages/NewApplicationPage.jsx";
-import EditApplicationPage from "./pages/EditApplicationPage.jsx"
-import LoginPage from "./pages/LoginPage.jsx"
-import SignupPage from "./pages/SignupPage.jsx"
-
+import EditApplicationPage from "./pages/EditApplicationPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Navigate to="/applications" replace />} />
-          <Route path="/applications" element={<ApplicationsPage />} />
-          <Route path="/applications/create" element={<NewApplicationPage />} />
-          <Route path="/applications/:id/edit" element={<EditApplicationPage />} />
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Navigate to="/applications" replace />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/applications"
+          element={
+            <ProtectedRoute>
+              <ApplicationsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/applications/create"
+          element={
+            <ProtectedRoute>
+              <NewApplicationPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/applications/:id/edit"
+          element={
+            <ProtectedRoute>
+              <EditApplicationPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
