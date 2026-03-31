816499d681b19bce8d83c3b001e4b8a5f99a22c7
Masoud Mahmoodi <mrm7g8@gmail.com>
2026-03-31 02:53:11 -0700

    solve

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -11,6 +11,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD || undefined,
   port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
